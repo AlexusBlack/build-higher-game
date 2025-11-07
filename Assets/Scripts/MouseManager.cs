f1c2ca5b15e45c744e90beda8fc18ca3d084e6f9
Alex Chernov <alexusblack@gmail.com>
2025-11-07 11:17:22 +1000

    support multiple box types

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    public GameObject prefabToPlace;   // Assign your prefab in the inspector
+    // public GameObject prefabToPlace;   // Assign your prefab in the inspector
+    public List<BoxType> boxTypes; // List of box types with weights
     public GameObject boxesParent;    // Parent object for organization
     private GameObject currentObject;  // The currently instantiated object
     private bool isPlacing = false;
 
     private GameManager gameManager;
+    private int totalWeight;
 
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        // Calculate total weight for random selection
+        foreach (var boxType in boxTypes)
+        {
+            totalWeight += boxType.useWeight;
+        }
     }
 
     // Update is called once per frame
@@ -38,9 +45,26 @@ public class MouseManager : MonoBehaviour
         }
     }
 
+    GameObject GetRandomBoxPrefab()
+    {
+        int randomValue = Random.Range(0, totalWeight);
+        int cumulativeWeight = 0;
+
+        foreach (var boxType in boxTypes)
+        {
+            cumulativeWeight += boxType.useWeight;
+            if (randomValue < cumulativeWeight)
+            {
+                return boxType.prefab;
+            }
+        }
+
+        return boxTypes[0].prefab; // Fallback
+    }
+
     void StartPlacing()
     {
-        currentObject = Instantiate(prefabToPlace, boxesParent.transform);
+        currentObject = Instantiate(GetRandomBoxPrefab(), boxesParent.transform);
         // disable rigidbody while placing
         currentObject.GetComponent<Rigidbody2D>().isKinematic = true;
         gameManager.AddBox();
